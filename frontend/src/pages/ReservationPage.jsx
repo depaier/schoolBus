31913f5cbb756e3df9697977b2a11e2054762058
depaier <depaier7@gmail.com>
2025-11-25 16:23:40 +0900

    feat: 예약조회 페이지 생성

---
+++ frontend/src/pages/ReservationPage.jsx
@@ -0,0 +1,13 @@
+import './ReservationPage.css'
+
+function ReservationPage() {
+  return (
+    <div className="reservation-page">
+      <div className="reservation-container">
+        <h1>예약조회</h1>
+      </div>
+    </div>
+  )
+}
+
+export default ReservationPage
