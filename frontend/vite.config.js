e0e33395ae7a8a2290af839ac9d02066edf79ada
depaier <depaier7@gmail.com>
2025-11-27 16:25:29 +0900

    ngrok을 위한 설정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      '.ngrok-free.app',
+      '.ngrok.io',
+      'localhost',
+      '127.0.0.1'
+    ],
+    allowHmrAccessFrom: [
+      'http://localhost:8000',
+      'http://127.0.0.1:8000',
+      'http://127.0.0.1:5173',
+      'http://localhost:5173',
+      'http://localhost:4040',
+    ],
+  },
 })
